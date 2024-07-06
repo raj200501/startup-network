5cf51dca45873c6fe8b5c126fba31bafc8f4e0a0
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-07-06 01:06:03 -0700

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Posts from './components/posts/PostList';
+
+import './App.css';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Route exact path="/" component={Posts} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/posts" component={Posts} />
+          </Switch>
+        </section>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
