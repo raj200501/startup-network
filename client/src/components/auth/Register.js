09b9b353dbe3b10457c8567611e849860feb24fa
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-07-06 01:03:45 -0700

    Create Register.js

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
+import PropTypes from 'prop-types';
+
+const Register = ({ setAlert, register, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      setAlert('Passwords do not match', 'danger');
+    } else {
+      register({ name, email, password });
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <div>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input type="text" placeholder="Name" name="name" value={name} onChange={onChange} required />
+        </div>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange={onChange} required />
+        </div>
+        <div className="form-group">
+          <input type="password" placeholder="Password" name="password" value={password} onChange={onChange} required />
+        </div>
+        <div className="form-group">
+          <input type="password" placeholder="Confirm Password" name="password2" value={password2} onChange={onChange} required />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </div>
+  );
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
